adf7c5b925c458d2408729f8ae5e0eede16c0d78
Josh-Cena <sidachen2003@gmail.com>
2021-10-16 12:18:07 +0800

    Finish build function

--- src/build.ts
+++ src/build.ts
@@ -1,25 +1,14 @@
-import path from 'path';
-import fs from 'fs';
-import { transformFileSync } from '@babel/core';
-import { globby } from 'globby';
-import prettier, { Options as PrettierOptions } from 'prettier';
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-export function compileOrCopy(
-  filePath: string,
-  sourceDir: string,
-  targetDir: string,
-  compileAction: (file: string) => string,
-): void {
-  const targetPath = path
-    .resolve(targetDir, path.relative(sourceDir, filePath))
-    .replace(/\.tsx?/g, '.js');
-  fs.mkdirSync(path.dirname(targetPath), { recursive: true });
-  if (/\.tsx?/.test(path.extname(filePath))) {
-    fs.writeFileSync(targetPath, compileAction(filePath));
-  } else {
-    fs.copyFileSync(filePath, targetPath);
-  }
-}
+import fs from 'fs';
+import {Globby} from '@docusaurus/utils';
+import Prettier from 'prettier';
+import {compileOrCopy, compileServerCode, compileClientCode} from './compiler';
 
 function transformDir(
   sourceDir: string,
@@ -27,35 +16,10 @@ function transformDir(
   compileAction: (file: string) => string,
   ignore: string[],
 ) {
-  globby(`${sourceDir}/**/*`, {
-    ignore: [...ignore, '**/*.d.ts'],
-  }).then((files) =>
-    files.forEach((filePath) => {
-      compileOrCopy(filePath, sourceDir, targetDir, compileAction);
-    }),
-  );
-}
-
-export function fullyTranspile(file: string): string {
-  return (
-    transformFileSync(file, {
-      presets: [['@babel/preset-typescript', { isTSX: true, allExtensions: true }]],
-      plugins: [
-        '@babel/plugin-transform-modules-commonjs',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-optional-chaining',
-      ],
-    })?.code ?? ''
-  );
-}
-
-function stripTypes(file: string, prettierConfig: PrettierOptions) {
-  // TODO let's hope for the pipeline operator :D
-  return prettier.format(
-    transformFileSync(file, {
-      presets: [['@babel/preset-typescript', { isTSX: true, allExtensions: true }]],
-    })?.code ?? '',
-    { parser: 'babel', ...prettierConfig },
+  Globby(`${sourceDir}/**/*`, {ignore}).then((files) =>
+    files.forEach((filePath) =>
+      compileOrCopy(filePath, sourceDir, targetDir, compileAction),
+    ),
   );
 }
 
@@ -72,20 +36,22 @@ export default async function build(
     sourceDir = 'src',
     targetDir = 'lib',
     themeDir = 'src/theme',
-    themeTargetDir = 'lib/js-theme',
+    themeTargetDir = 'lib/theme',
     ignore = ['**/__tests__/**'],
   } = options;
   // Compile: src/*.ts -> lib/*.js
-  transformDir(sourceDir, targetDir, fullyTranspile, [...ignore, '**/*.d.ts']);
-  // Re-compile & prettier: src/theme/*.tsx -> lib/js-theme/*.js (for swizzling)
+  transformDir(sourceDir, targetDir, compileServerCode, [...ignore, '**/*.d.ts']);
+  // Strip types & prettier: src/theme/*.tsx -> lib/theme/*.js (client code will be swizzlable)
   if (fs.existsSync(themeDir)) {
-    const prettierConfig = await prettier.resolveConfig(themeDir);
+    const prettierConfig = await Prettier.resolveConfig(themeDir);
     if (!prettierConfig) {
-      throw new Error('Prettier config file not found');
+      throw new Error('Prettier config file not found. Building the theme code requires using Prettier to format the JS code, which will be used for swizzling.');
     }
-    transformDir(themeDir, themeTargetDir, (file) => stripTypes(file, prettierConfig), [
-      ...ignore,
-      '**/*.d.ts',
-    ]);
+    transformDir(
+      themeDir,
+      themeTargetDir,
+      (file) => compileClientCode(file, prettierConfig),
+      [...ignore, '**/*.d.ts'],
+    );
   }
 }
--- src/watch.ts
+++ src/watch.ts
@@ -1,27 +1,48 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import chokidar from 'chokidar';
-import { debounce } from 'lodash';
+import {debounce} from 'lodash';
 import chalk from 'chalk';
-import { fullyTranspile, compileOrCopy } from './build';
+import {compileOrCopy, compileClientCode, compileServerCode} from './compiler';
 
 export default async function watch(
   options: Partial<{
     sourceDir: string;
     targetDir: string;
+    themeDir: string;
+    themeTargetDir: string;
     ignore: string[];
   }> = {},
 ): Promise<void> {
-  const { sourceDir = 'src', targetDir = 'lib', ignore = ['**/__tests__/**'] } = options;
-  const watcher = chokidar.watch(sourceDir, {
+  const {
+    sourceDir = 'src',
+    targetDir = 'lib',
+    themeDir = 'src/theme',
+    themeTargetDir = 'lib/theme',
+    ignore = ['**/__tests__/**'],
+  } = options;
+  const watcher = chokidar.watch([sourceDir, themeDir], {
     ignoreInitial: true,
-    ignored: ignore,
+    ignored: [...ignore, '**/*.d.ts'],
     awaitWriteFinish: {
       stabilityThreshold: 50,
       pollInterval: 10,
     },
   });
-  const debouncedCompile = debounce((filePath) => {
+  const compile = debounce((filePath: string) => {
     try {
-      compileOrCopy(filePath, sourceDir, targetDir, fullyTranspile);
+      // TODO: is check this good enough?
+      if (filePath.includes(themeDir)) {
+        // For perf reasons, we don't do prettier in watch mode
+        compileOrCopy(filePath, themeDir, themeTargetDir, compileClientCode);
+      } else {
+        compileOrCopy(filePath, sourceDir, targetDir, compileServerCode);
+      }
     } catch (e) {
       console.log(chalk.red(`Error while processing ${chalk.cyan(filePath)}:`));
       console.error(e);
@@ -30,9 +51,13 @@ export default async function watch(
 
   ['add', 'change'].forEach((event) => {
     watcher.on(event, async (filePath: string) => {
-      debouncedCompile(filePath);
-      console.log(chalk.green(`Compilation of ${chalk.cyan(filePath)} finished`));
+      compile(filePath);
+      console.log(
+        chalk.green(`Compilation of ${chalk.cyan(filePath)} finished`),
+      );
     });
   });
-  console.log(chalk.green(`Watching file changes in ${chalk.cyan(sourceDir)}...`));
+  console.log(
+    chalk.green(`Watching file changes in ${chalk.cyan(sourceDir)}...`),
+  );
 }
